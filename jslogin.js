743c8025bde671ab1cfa626f0e56d1a2e5c3cde2
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2022-10-11 23:20:43 -0400

    JS Form Example on Android

---
+++ jslogin.js
@@ -0,0 +1,27 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have "+attempt+" attempt left.");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
+
+function clearForm() {
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+}
